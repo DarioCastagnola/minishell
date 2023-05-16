db5299403c223284971eae23f2485d7545f376b9
dcastagn42 <112878659+DarioCastagnola@users.noreply.github.com>
2023-05-16 11:40:37 +0200

    Add files via upload

---
+++ minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcastagn <dcastagn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/05 11:06:26 by dcastagn          #+#    #+#             */
+/*   Updated: 2023/05/09 14:40:18 by dcastagn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <fcntl.h>
+# define RED "\033[0;31m"
+# define END "\033[0m"
+# define CYAN "\033[0;36m"
+
+#endif
